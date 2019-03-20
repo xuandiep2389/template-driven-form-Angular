7ab30c99b9c8ca50f233670ce4ef98b6351584b6
vu linh <xuandiep2389@gmail.com>
2019-03-20 12:02:31 +0700

    template form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroFormComponent } from './hero-form/hero-form.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { HeroFormComponent } from './hero-form/hero-form.component';
     HeroFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
   selector: 'app-hero-form',
@@ -10,6 +11,24 @@ export class HeroFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+
+  powers = ['really smart', 'super flexible', 'super hot', 'weather changer'];
+
+  model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
+
+  submitted = false;
+
+  onSubmit(){
+    this.submitted = true;
+  }
+
+  //TODO : Remove this when we're done
+  get diagnostic() {return JSON.stringify(this.model)}
+
+  newHero() {
+    this.model = new Hero(31, '','')
   }
 
 }
