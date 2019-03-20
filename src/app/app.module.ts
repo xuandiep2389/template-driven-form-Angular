7047c322cdab3c1ed7971644dfd21bcae3eda722
vu linh <xuandiep2389@gmail.com>
2019-03-20 10:55:05 +0700

    hien thi thong tin cua hero ra form o view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroFormComponent
   ],
   imports: [
     BrowserModule
